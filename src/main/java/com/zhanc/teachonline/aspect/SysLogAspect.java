297d38effa87fe641a60e76b10a2f36b56e9d866
zhanc <875759270@qq.com>
2022-03-26 14:47:51 +0800

    日志切片类

---
+++ src/main/java/com/zhanc/teachonline/aspect/SysLogAspect.java
@@ -0,0 +1,101 @@
+package com.zhanc.teachonline.aspect;
+
+import com.alibaba.fastjson.JSONObject;
+import com.zhanc.teachonline.annotation.MyLog;
+import com.zhanc.teachonline.entity.SysLog;
+import com.zhanc.teachonline.service.SysLogService;
+import com.zhanc.teachonline.utils.CommonUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.lang.reflect.Method;
+import java.util.Date;
+
+/**
+ * 系统日志：切面处理类
+ */
+@Aspect
+@Component
+public class SysLogAspect {
+
+    @Autowired
+    private SysLogService sysLogService;
+
+    //定义切点 @Pointcut
+    //在注解的位置切入代码
+    @Pointcut("@annotation( com.zhanc.teachonline.annotation.MyLog)")
+    public void logPointCut() {
+    }
+
+    //切面 配置通知
+    @AfterReturning("logPointCut()")
+    public void saveSysLog(JoinPoint joinPoint) {
+        System.out.println("操作已记录至数据库");
+        //保存日志
+        SysLog sysLog = new SysLog();
+
+        //从切面织入点处通过反射机制获取织入点处的方法
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        //获取切入点所在的方法
+        Method method = signature.getMethod();
+
+        //获取操作
+        MyLog myLog = method.getAnnotation(MyLog.class);
+        if (myLog != null) {
+            String value = myLog.value();
+            sysLog.setOperation(value);//保存获取的操作
+        }
+
+        //获取请求的类名
+        String className = joinPoint.getTarget().getClass().getName();
+        //获取请求的方法名
+        String methodName = method.getName();
+        sysLog.setMethod(className + "." + methodName);
+
+        //请求的参数
+        Object[] args = joinPoint.getArgs();
+        Object[] arguments  = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i] instanceof ServletRequest || args[i] instanceof ServletResponse || args[i] instanceof MultipartFile) {
+                //ServletRequest不能序列化，从入参里排除，否则报异常：java.lang.IllegalStateException: It is illegal to call this method if the current request is not in asynchronous mode (i.e. isAsyncStarted() returns false)
+                //ServletResponse不能序列化 从入参里排除，否则报异常：java.lang.IllegalStateException: getOutputStream() has already been called for this response
+                continue;
+            }
+            arguments[i] = args[i];
+        }
+        String paramter = "";
+        if (arguments != null) {
+            try {
+                paramter = JSONObject.toJSONString(arguments);
+            } catch (Exception e) {
+                paramter = arguments.toString();
+            }
+        }
+        sysLog.setParams(paramter);
+
+        sysLog.setCreateTime(new Date());
+
+        //获取用户ip地址
+        HttpServletRequest request = CommonUtils.getHttpServletRequest();
+        HttpSession session=request.getSession();
+        //获取用户名
+        //sysLog.setUserName((String) session.getAttribute("userName"));
+        sysLog.setUserName(("userName"));
+
+        sysLog.setIp(CommonUtils.getIpAddr(request));
+
+        //调用service保存SysLog实体类到数据库
+        sysLogService.save(sysLog);
+    }
+
+}
